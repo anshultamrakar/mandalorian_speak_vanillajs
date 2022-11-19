ed99428f7f42c04ad44685a522e8c73df273930a
Anshul Tamrakar <anshultamrakar487@gmail.com>
2022-11-19 14:33:48 +0530

    final commit

--- main.js
+++ main.js
@@ -1,11 +1,36 @@
 
-var btnTranslate  = document.querySelectorAll("#btn-translate")
+var btnTranslate  = document.querySelector("#btn-translate")
+var textInput = document.querySelector("#input-txt")
+var outputText = document.querySelector("#output")
 
-var txtInput = document.querySelector("#input-txt");
+ var serverUrl = "https://api.funtranslations.com/translate/mandalorian.json"
+
+
+function getTranslationApp(text){
+    return serverUrl + "?" + "text=" + text
+}
+
+
+function errorHandler(error){
+    console.log(error)
+}
 
 
 function clickHandler(){
-    console.log("hello")
+    var inputText = textInput.value
+    fetch(getTranslationApp(inputText))
+    .then(response => response.json())
+    .then(json => output.innerHTML = json.contents.translated)
+    .catch(errorHandler)
 }
 
-btnTranslate.addEventListener("click", clickHandler) 
+
+
+
+btnTranslate.addEventListener("click", clickHandler)
+
+
+
+
+
+
